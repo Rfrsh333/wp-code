9b196d0c18a0c8e6bff861ffef35b28e23da0f10
Rfrsh333 <247587816+Rfrsh333@users.noreply.github.com>
2025-12-22 05:24:31 +0100

    Allow GTM and analytics in CSP

--- next.config.ts
+++ next.config.ts
@@ -36,12 +36,12 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://www.googletagmanager.com https://www.google-analytics.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "img-src 'self' data: blob: https:",
-      "font-src 'self' https://fonts.gstatic.com",
+      "font-src 'self' data: https://fonts.gstatic.com",
       "frame-src https://www.google.com",
-      "connect-src 'self' https://www.google.com https://*.supabase.co",
+      "connect-src 'self' https://www.google.com https://www.googletagmanager.com https://www.google-analytics.com https://analytics.google.com https://*.supabase.co",
     ].join('; '),
   },
 ];
